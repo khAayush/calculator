f67e72dc1584b81ad1dca6c8743c611b77be37b1
Rubyyish <ishakarki704@gmail.com>
2025-03-24 07:59:31 +0545

    Added sum feature

--- calculator.py
+++ calculator.py
@@ -1,6 +1,18 @@
+
 def main():
     print("Welcome to the calculator")
     print("\nChoose one number:\n")
     print("1. Addition\n2. Substraction\n3. Multiplication\n4. Division")
     ch = int(input("Enter your number for desired operation: "))
     
+# Sum of two numbers
+def sum():
+    num1= float(input("Enter first number: "))
+    num2= float(input("Enter another number: "))
+    if num1 <=0 or num2 <= 0:
+        res = num1-num2
+    
+    else:
+        res = num1+num2
+    print(f"The sum is {res}")
+sum()
