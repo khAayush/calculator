81d36214ffedead33e84431b88a118b213e0c4c5
khAayush <khatiwadaprince1@gmail.com>
2025-03-24 07:29:50 +0545

    Created Calculator code file

---
+++ calculator.py
@@ -0,0 +1,6 @@
+def main():
+    print("Welcome to the calculator")
+    print("\nChoose one number:\n")
+    print("1. Addition\n2. Substraction\n3. Multiplication\n4. Division")
+    ch = int(input("Enter your number for desired operation: "))
+    
