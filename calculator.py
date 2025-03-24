a65fe9586b7ffed44e812fd844269ad77e6d7bdc
BishantRajbanshi <rajbancbishant@gmail.com>
2025-03-24 07:41:40 +0545

    added substraction feature

--- calculator.py
+++ calculator.py
@@ -1,3 +1,10 @@
+def sub_two_numbers():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    print("The sub is:", num1-num2)
+
+
+
 def main():
     print("Welcome to the calculator")
     print("\nChoose one number:\n")
