bfb0b1b408d3f830e49d02cd81b0e87b607ff717
khAayush <khatiwadaprince1@gmail.com>
2025-03-24 08:07:19 +0545

    Merged two requests

--- calculator.py
+++ calculator.py
@@ -1,10 +1,3 @@
-
-def main():
-    print("Welcome to the calculator")
-    print("\nChoose one number:\n")
-    print("1. Addition\n2. Substraction\n3. Multiplication\n4. Division")
-    ch = int(input("Enter your number for desired operation: "))
-    
 # Sum of two numbers
 def sum():
     num1= float(input("Enter first number: "))
@@ -16,3 +9,15 @@ def sum():
         res = num1+num2
     print(f"The sum is {res}")
 sum()
+
+def sub_two_numbers():
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    print("The sub is:", num1-num2)
+
+def main():
+    print("Welcome to the calculator")
+    print("\nChoose one number:\n")
+    print("1. Addition\n2. Substraction\n3. Multiplication\n4. Division")
+    ch = int(input("Enter your number for desired operation: "))
+    
